14bddfdebfdd9939db31cb30df0c90027d8be812
MSH512 <onlinecerticate12@gmail.com>
2026-08-30 15:55:33 +0500

    Update auth-utils.js

--- school-site/js/auth-utils.js
+++ school-site/js/auth-utils.js
@@ -7,3 +7,4 @@ export async function sha256Hex(text) {
   const buf = await crypto.subtle.digest("SHA-256", enc);
   return Array.from(new Uint8Array(buf)).map((b) => b.toString(16).padStart(2, "0")).join("");
 }
+
